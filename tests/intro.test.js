f0823adfcc25d657bb602f1c77e20a6794ccdf5e
Bartosz Art <bartosz.art@gmail.com>
2024-11-24 16:40:49 +0100

    Test: add unit test for fizzBuzz() function

--- tests/intro.test.js
+++ tests/intro.test.js
@@ -1,5 +1,5 @@
 import { describe, it, expect } from 'vitest';
-import { max } from '../src/intro';
+import { fizzBuzz, max } from '../src/intro';
 
 describe('max', () => {
 	it('should return the first argument if it is greater', () => {
@@ -15,5 +15,20 @@ describe('max', () => {
 	});
 	it('should return the first argument if argument are equal', () => {
 		expect(max(1, 1)).toBe(1);
+	});
+});
+
+describe('fizzBuzz', () => {
+	it('should return "FizzBuzz" if the argument is devisible by 3 and 5', () => {
+		expect(fizzBuzz(15)).toBe('FizzBuzz');
+	});
+	it('should return "Fizz" if the argument is only devisible by 3', () => {
+		expect(fizzBuzz(3)).toBe('Fizz');
+	});
+	it('should return "Buzz" if the argument is only devisible by 5', () => {
+		expect(fizzBuzz(5)).toBe('Buzz');
+	});
+	it('should return argument as a string if it is not devisible by 3 or 5', () => {
+		expect(fizzBuzz(1)).toBe('1');
 	});
 });
