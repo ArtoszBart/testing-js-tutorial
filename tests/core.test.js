5238c47d407aa3090eea120a9388cfc0bdc0c59b
Bartosz Art <bartosz.art@gmail.com>
2024-11-25 17:39:35 +0100

    Test: add unit test for fetch() async function

--- tests/core.test.js
+++ tests/core.test.js
@@ -2,6 +2,7 @@ import { describe, it, expect } from 'vitest';
 import {
 	calculateDiscount,
 	canDrive,
+	fetchData,
 	getCoupons,
 	isPriceInRange,
 	isValidUsername,
@@ -140,4 +141,17 @@ describe('canDrive', () => {
 			expect(canDrive(age, country)).toBe(result);
 		}
 	);
+});
+
+describe('fetchData', () => {
+	it('should return a promise that will resolve to an array of numbers', async () => {
+		try {
+			const result = await fetchData();
+			expect(Array.isArray(result)).toBe(true);
+			expect(result.length).toBeGreaterThan(0);
+		} catch (error) {
+			expect(error).toHaveProperty('reason');
+			expect(error.reason).toMatch(/fail/i);
+		}
+	});
 });
