d2e963c568f998ea3bdef77d896e196eb844ff51
Bartosz Art <bartosz.art@gmail.com>
2024-11-26 14:29:46 +0100

    Test: add unit test for getPriceInCurrency() function

--- tests/mocking.test.js
+++ tests/mocking.test.js
@@ -1,4 +1,8 @@
 import { vi, it, expect, describe } from 'vitest';
+import { getPriceInCurrency } from '../src/mocking';
+import { getExchangeRate } from '../src/libs/currency';
+
+vi.mock('../src/libs/currency');
 
 describe('test suite', () => {
 	it('test case', () => {
@@ -9,5 +13,15 @@ describe('test suite', () => {
 
 		expect(sendText).toHaveBeenCalledWith('message');
 		expect(result).toBe('ok');
+	});
+});
+
+describe('getPriceInCurrency', () => {
+	it('should return price in target currency', () => {
+		vi.mocked(getExchangeRate).mockReturnValue(1.5);
+
+		const price = getPriceInCurrency(10, 'PLN');
+
+		expect(price).toBe(15);
 	});
 });
